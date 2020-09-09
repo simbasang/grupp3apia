b7ab3bf3eeec2321c5b3c0efad5f45dd40da6760
simbasang <sebastian.englund@live.se>
2020-09-09 14:19:03 +0200

    test added

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace grupp3api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public string GetHello()
+        { 
+            return "Hello";
+        }
+    }
+}
